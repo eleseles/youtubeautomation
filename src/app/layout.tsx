6c0811889cda42ed4477c04a117b0329e23549a0
Ramazan Eles <ramazaneles86@gmail.com>
2026-03-27 19:26:36 +0300

    brand: update favicon and apple-icon icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,9 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.png",
+    shortcut: "/favicon.png",
+    apple: "/apple-icon.png",
   },
   verification: {
     google: "b0HvVxoM08vCf4XpUbzPnubPIPQagn8HX2CoRr1H3e0",
